0caa5cb3a25bfb5b243595e9108d3a48cb8388c6
jrucker <jrucker@163.com>
2019-02-15 14:21:46 +0800

    [fix] update

--- config/api.js
+++ config/api.js
@@ -1,10 +1,10 @@
 const build = {
-  static_url: 'http://blog.jrucker.cn/', // 静态资源路径
-  common_url: 'http://api.jrucker.cn',
+  static_url: 'http://static-client.jrucker.cn/', // 静态资源路径
+  common_url: 'http://www.jrucker.cn',
 };
 const build_dev = {
-  static_url: 'http://blog.jrucker.cn:8081/', // 静态资源路径
-  common_url: 'http://api.jrucker.cn:8081',
+  static_url: 'http://static-client.jrucker.cn:8081/', // 静态资源路径
+  common_url: 'http://www.jrucker.cn:7777',
 };
 const dev = {
   static_url: 'http://localhost:8081/', // 静态资源路径
