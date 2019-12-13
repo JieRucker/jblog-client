9fac1dca68882157ccac2f24c55d98448d72c87b
JieRucker <1435255141@qq.com>
2019-12-13 16:33:00 +0800

    [fix] compose

--- deploy.sh
+++ deploy.sh
@@ -38,17 +38,20 @@
 #docker run -it -d --name jblog-client -p 8081:8081 jblog-client:v1.0
 
 # 关闭容器
-docker-compose stop || true;
+docker-compose stop
 # 删除容器
-docker-compose down || true;
+docker-compose down
+# 删除卷
+docker volume rm bimpb_web
+docker volume ls
 # 构建镜像
-docker-compose build;
+docker-compose build
 # 启动并后台运行
-docker-compose up -d;
+docker-compose up -d
 # 查看日志
 #docker logs nodejs;
 # 对空间进行自动清理
-docker system prune -a -f
+#docker system prune -a -f
 
 
 
