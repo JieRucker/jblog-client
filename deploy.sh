06c72ef20939dfb218f230b5d756df505b80d092
JieRucker <1435255141@qq.com>
2019-12-15 23:39:46 +0800

    [fix] deploy

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 
-#PROJECT_NAME = "jblog-client" # 项目名称
-#IMAGE_VERSION = "v1.0" # 镜像版本
-
-#RESUME_PATH
-
-#cd /home
-#
-## 创建项目代码的目录
-#echo "创建项目目录"
-#mkdir -p work
-#
-#echo "克隆新代码"
-#git clone git@47.99.68.87:/home/git/blog/jblog-client.git
-#
-#cd jblog-client
-#
-#echo "编译build"
-#npm run build
-#
-#echo "安装node镜像：10.7.0"
-#docker pull node:10.7.0
-#
-#echo "安装nginx镜像：1.12.2"
-#docker pull nginx:1.12.2
-#
 #echo "制作镜像:jblog-client"
 #docker build -t jblog-client:v1.0 .
-#
+
 #echo "停止旧容器，并删除旧容器"
 #docker stop jblog-client_web_container
 #docker rm jblog-client_web_container
@@ -37,26 +12,44 @@
 #echo "启动新容器"
 #docker run -it -d --name jblog-client -p 8081:8081 jblog-client:v1.0
 
-# "https://m2ipce2n.mirror.aliyuncs.com",
-# "https://registry.docker-cn.com",
-# "http://f1361db2.m.daocloud.io"
+#!/bin/bash
 
-# 关闭容器
-docker-compose stop
-# 删除容器
+server_address="47.99.68.87"
+folder="/home/work/blog"
+project_folder="/home/work/blog/jblog-client"
+
+if [ ! -d "$folder" ]; then
+  echo '创建目录：'$folder''
+  mkdir -p "$folder"
+fi
+
+echo '进入'$folder'目录：'
+cd $folder
+
+if [ -d "$project_folder" ]; then
+  cd $project_folder
+  git pull origin master
+else
+  echo "克隆代码："
+  git clone git@$server_address:/home/git/blog/jblog-client.git
+  cd $project_folder
+fi
+
 docker-compose down
-#docker-compose rm -f
-# 删除卷
+docker volume rm jblogclient_web
 docker volume rm jblog-client_web
 docker volume ls
-# 构建镜像
 docker-compose build
-# 启动并后台运行
 docker-compose up -d
 # 查看日志
 #docker logs jblog-client_web_container;
 # 对空间进行自动清理
 #docker system prune -a -f
+
+
+
+
+
 
 
 
