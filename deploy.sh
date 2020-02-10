dfb70b570ab583570294edccb540a4c195f209e2
JieRucker <1435255141@qq.com>
2020-02-10 22:07:29 +0800

    [fix] deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
-docker cp jblog-client_web_container:/home/jblog-client/dist /home/git/blog/work/jblog-client/dist
+docker cp jblog-client_container:/home/jblog-client/dist /home/git/blog/work/jblog-client/dist
 docker cp /home/git/blog/work/jblog-client/dist nginx_container:/usr/share/nginx/html/jblog-client
 echo "构建成功"
 
