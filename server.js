1a2951a3277444f87ca8d65986822c7994c0ca03
JieRucker <1435255141@qq.com>
2020-03-14 16:19:40 +0800

    [fix] update

--- server.js
+++ server.js
@@ -92,7 +92,6 @@ app.use('/service-worker.js', serve('./dist/static/service-worker.js'));
 // since this app has no user-specific content, every page is micro-cacheable.
 // if your app involves user-specific content, you need to implement custom
 // logic to determine whether a request is cacheable based on its url and
-// headers.
 // 1-second microcache.
 // https://www.nginx.com/blog/benefits-of-microcaching-nginx/
 app.use(microcache.cacheSeconds(1, req => useMicroCache && req.originalUrl));
