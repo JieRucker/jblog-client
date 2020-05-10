4f86d3dc2ed0f33d0e3cdc7b42145777bae038ff
JieRucker <1435255141@qq.com>
2020-05-10 21:00:58 +0800

    [fix] 修改

--- server.js
+++ server.js
@@ -103,7 +103,6 @@ function render(req, res) {
     } else if (err.code === 404) {
       res.status(404).send('404 | Page Not Found')
     } else {
-      // Render Error Page or Redirect
       res.status(500).send('500 | Internal Server Error');
       console.error(`error during render : ${req.url}`);
       console.error(err.stack)
