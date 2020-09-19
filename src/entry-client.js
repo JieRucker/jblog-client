2e18507c877ab3e9478181119cc51b20f2e80507
JieRucker <1435255141@qq.com>
2020-09-19 14:29:59 +0800

    [fix] update

--- src/entry-client.js
+++ src/entry-client.js
@@ -25,13 +25,11 @@ Vue.mixin({
 
 const { app, router, store } = createApp();
 
-// prime the store with server-initialized state.
 // the state is determined during SSR and inlined in the page markup.
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
 
-// wait until router has resolved all async before hooks
 // and async components...
 router.onReady(() => {
   // Add router hook for handling asyncData.
