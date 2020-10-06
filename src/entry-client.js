b71e352d5e79a6bd1f1ac5b76514364854df2877
JieRucker <1435255141@qq.com>
2020-10-06 20:50:54 +0800

    [fix] update

--- src/entry-client.js
+++ src/entry-client.js
@@ -4,7 +4,7 @@ import { createApp } from './main'
 import ProgressBar from './components/ProgressBar.vue'
 
 // global progress bar
-const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount()
+const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount();
 document.body.appendChild(bar.$el)
 
 // a global mixin that calls `asyncData` when a route component's params change
